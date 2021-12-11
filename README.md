a002b9a3a273829ba5cb37f12f3eec42846f5550
ahmedsanad88 <57454543+ahmedsanad88@users.noreply.github.com>
2021-12-11 16:22:52 +0400

    Template 1

---
+++ README.md
@@ -0,0 +1,53 @@
+# Website Template 1:
+
+## Mobile friendly
+
+## Inspired by PSD design from graphberry full details will be on the below link:
+
+    [Check PSD Design](https://www.graphberry.com/item/kasper-one-page-psd-template)
+
+## ALL Images used from [pexels](pexels.com)
+
+
+Clean and beautiful template development can be used as a basic structure to get fast results from your target site.
+
+Includes:
+    - Landing page: which will tell a simple story about the company.
+    - Services: This tells the client how this company will help him to turn his ideas into a real and live project.
+    - Portfolio: which showcases for clients.
+    - About: Tell about the company start and experience.
+    - Pricing: Which used a price list for the clients to help him decide which of which will be enough for his needs.
+    - Contact: This will help the customer to simply take an action and contact the company for consultation.
+    - And more ...
+
+The whole site is based on HTML and CSS and some functionality from javascript which will be ready to be updated by your idea or add more details for sections.
+The design is responsive for small & middle and bigger devices.
+
+
+# How it's looking on live?
+
+
+[Golive](https://ahmedsanad88.github.io/Web-Development-Company-Site/)
+
+
+
+## Technology and Tools Used:
+
+`HTML5,
+CSS3,
+normalize,
+Font Awesome,
+javascript.`
+
+
+## Contributing
+
+Pull requests are welcome.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+# Author
+
+`Ahmed Sanad`
---
+++ app.js
@@ -0,0 +1,145 @@
+const list = document.querySelectorAll('.shuffle-list');
+
+list.forEach((item) => item.addEventListener('click', (e) => {
+  // console.log(e.target);
+  removeClass(list);
+  e.target.classList.add('active');
+}));
+
+// Function to remove all class from the shuffle list.
+
+const removeClass = (group) => {
+  group.forEach((item) => item.classList.remove('active'));
+};
+
+// show or hide btn for scroll.
+window.onscroll = function() {scrollFunction()};
+
+const btn = document.getElementById('button');
+
+// function to show or disappear the back to top bottom.
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+}
+
+// back to top function
+
+btn.addEventListener('click', topFunction);
+
+function topFunction() {
+    // Using shortcut by using scrollTo.
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    })
+
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+}
+
+
+// Mobile menu control
+
+const openMenu = document.getElementById('open-menu');
+// const closeMenu = document.getElementById('close-menu');
+const navList = document.getElementById('navList');
+
+openMenu.addEventListener('click', () => {
+  openMenu.classList.toggle('pop-animation');
+  navList.classList.toggle('show-navList');
+  if (navList.classList.contains('show-navList')) {
+    navList.style.display="flex";
+  }else {
+    navList.style.display="none";
+  }
+});
+
+// resize window listener to fix display fix when transfer from and to small size.
+
+window.addEventListener('resize', function(event){
+    if(window.innerWidth > 767) {
+      navList.classList.remove('show-navList');
+      navList.style.display= "flex";
+    }
+    else {
+      navList.style.display= "none";
+      openMenu.classList.remove('hide-menuBar');
+    }
+});
+
+
+// shuffle list filter the list to show special or related item for the list.
+
+const shuffleList = document.querySelectorAll('.shuffle-list');
+const boxes = document.querySelectorAll('.image-box');
+
+shuffleList.forEach((item) => {
+  item.addEventListener('click', () => {
+    switch (item.innerHTML) {
+      case 'App':
+        checkClass('app');
+        break;
+
+      case 'Photo':
+        checkClass('photo');
+        break;
+
+      case 'Web':
+        checkClass('web');
+        break;
+
+      case 'Print':
+        checkClass('print');
+        break;
+    
+      default:
+        // console.log("Select all of them");
+        boxes.forEach((box) => {
+          box.classList.remove('box-animation');
+          setTimeout(() => {
+            box.classList.add('box-animation');
+            box.style.display='block';
+          }, 300)
+        });
+        break;
+    }
+  });
+});
+
+const checkClass = (testClass) => {
+  boxes.forEach((box) => {
+          if (box.classList.contains(testClass)) {
+            box.style.display='block';
+            box.classList.add('box-animation');
+          } else {
+            box.style.display='none';
+          }
+        });
+}
+
+
+// Show search page
+
+const searchIcon = document.querySelector('.searchIcon');
+const searchPage = document.querySelector('.search-page');
+
+searchIcon.addEventListener('click', () => {
+  if (searchPage.style.display === 'none') {
+    searchPage.classList.remove('searchRemove');
+    searchPage.style.display='flex';
+  }
+  else {
+    searchPage.classList.remove('searchRemove');
+    searchPage.classList.add('searchRemove');
+    setTimeout(() => {
+      searchPage.style.display='none';
+    }, 300)
+  }
+});
+
+
+
